f4b3c8cdff7a96bd994ba1ee426716f28e48ae1f
Developer Formerly Known as Decat <62384085+gatrivi@users.noreply.github.com>
2024-12-22 17:12:47 -0300

    initial commit - setup spring boot

---
+++ src/main/java/com/devtrivi/hogarth_prep/HogarthPrepApplication.java
@@ -0,0 +1,13 @@
+package com.devtrivi.hogarth_prep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HogarthPrepApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HogarthPrepApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devtrivi/hogarth_prep/HogarthPrepApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devtrivi.hogarth_prep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HogarthPrepApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
